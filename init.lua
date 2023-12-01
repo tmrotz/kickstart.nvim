e12aa3ed6921ccf3764592e67871527e1c673fcb
Travis Rotz <travis.rotz@daugherty.com>
2023-12-01 14:28:40 -0600

    Add desc to qfixlist commands

--- init.lua
+++ init.lua
@@ -305,18 +305,18 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', 'J', "mzJ`z")
 
 -- While half page jumping, stay centered
---vim.keymap.set('n', '<C-d>', "<C-d>zz")
---vim.keymap.set('n', '<C-u>', "<C-u>zz")
+vim.keymap.set('n', '<C-d>', "<C-d>zz")
+vim.keymap.set('n', '<C-u>', "<C-u>zz")
 
 -- While searching, keep found text in center of screen
 --vim.keymap.set('n', 'n', "nzzzv")
 --vim.keymap.set('n', 'N', "Nzzzv")
 
 
---vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
---vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
---vim.keymap.set('n', '<leader>k>', '<cmd>lnext<CR>zz')
---vim.keymap.set('n', '<leader>j>', '<cmd>lprev<CR>zz')
+vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz', { desc = "Forward qfixlist" })
+vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz', { desc = "Backward qfixlist" })
+--vim.keymap.set('n', '<leader>k>', '<cmd>lnext<CR>zz', { desc = "Forward location list" })
+--vim.keymap.set('n', '<leader>j>', '<cmd>lprev<CR>zz', { desc = "Backward location list" })
 
 
 
