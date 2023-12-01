80e8031dfd5dc1376a313af9a0b8f3b24550ff2a
Travis Rotz <travis.rotz@daugherty.com>
2023-12-01 14:24:41 -0600

    Add indent rainbow plugin to custom

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = function(_, opts)
+      -- Other blankline configuration here
+      return require("indent-rainbowline").make_opts(opts)
+    end,
+    dependencies = {
+      "TheGLander/indent-rainbowline.nvim",
+    },
+  },
+}
+
