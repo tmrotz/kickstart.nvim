fdd54912132645a70ec9345a01f30c0cdfcef8e3
Travis Rotz <tmrotz@gmail.com>
2024-02-13 13:19:41 -0600

    Enable Rust analyzer

--- init.lua
+++ init.lua
@@ -501,7 +501,7 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
