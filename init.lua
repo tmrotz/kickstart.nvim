cdde31eb385f9a7abaaec484aa589e1bd1989fbd
Travis Rotz <tmrotz@gmail.com>
2024-05-17 15:30:47 -0500

    Customize which-key settings

--- init.lua
+++ init.lua
@@ -493,14 +493,18 @@ end
 
 -- document existing key chains
 require('which-key').register({
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>gh'] = { name = '[H]unk', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  ['<leader>'] = {
+    c = { name = '[C]ode', _ = 'which_key_ignore' },
+    d = { name = '[D]ocument', _ = 'which_key_ignore' },
+    g = {
+      name = '[G]it',
+      h = { name = '[H]unk', _ = 'which_key_ignore' },
+    },
+    h = { name = '[H]arpoon' },
+    r = { name = '[R]ename', _ = 'which_key_ignore' },
+    s = { name = '[S]earch' },
+    w = { name = '[W]orkspace', _ = 'which_key_ignore' },
+  },
 })
 
 -- Enable the following language servers
