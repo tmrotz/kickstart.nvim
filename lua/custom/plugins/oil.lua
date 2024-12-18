24651e48f42aaf4aa6e81925df4d5297d6311188
Travis Rotz <tmrotz@gmail.com>
2024-12-18 14:56:15 -0600

    Get Oil working again

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -2,13 +2,14 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.setupOpts
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  keys = {
-    { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
-  },
   opts = {
     view_options = {
       show_hidden = true,
     },
+  },
+  lazy = false,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  keys = {
+    { '-', '<CMD>Oil<CR>', desc = 'Open parent directory' },
   },
 }
