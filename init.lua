e1fed218d04b825beb38c3695546accd55320d56
Travis Rotz <tmrotz@gmail.com>
2025-01-07 12:58:31 -0600

    Add support for golang

--- init.lua
+++ init.lua
@@ -623,7 +623,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        -- gopls = {},
+        gopls = {},
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -926,6 +926,7 @@ require('lazy').setup({
         'zig',
         'python',
         'javascript',
+        'go',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
